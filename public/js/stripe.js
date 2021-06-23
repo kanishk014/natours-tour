aac9c0d467acffd51fdbddcb4e1512118a8a0384
kanishk <kanishk.9871@gmail.com>
2021-06-23 20:33:15 +0530

    Initial commit

---
+++ public/js/stripe.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+export const bookTour = async (tourID) => {
+  const stripe = Stripe(
+    'pk_test_51J4Kz8SHGufhIZqgIWmxgdrE20A6C7svKlBCUFc4ojVdtQ9YuaUOhgpH7y3Jvtyw8Iv2f8Rbn3y6PBKZBn60EatG00Wk9mBTUk'
+  );
+
+  try {
+    // 1. Get the checkout session from API
+    const session = await axios(
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourID}`
+    );
+
+    console.log(session);
+
+    // 2. Create checkout form + charge credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
