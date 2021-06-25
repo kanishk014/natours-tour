ee2bd860eef341bcd551928e86740bcc4a68cb0d
kanishk <kanishk.9871@gmail.com>
2021-06-25 22:14:15 +0530

    Improved App.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
-const bookingController = require('./controlllers/bookingController');
+const bookingController = require('./controllers/bookingController');
 const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
