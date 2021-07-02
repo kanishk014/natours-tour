3b5da9d999489dc874a30d1c347cd21f0f53fc56
kanishk <kanishk.9871@gmail.com>
2021-07-02 17:37:34 +0530

    Updated Login Route

--- controllers/authController.js
+++ controllers/authController.js
@@ -93,7 +93,13 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError('Please provide email and password!', 400));
   }
   // 2) Check if user exists && password is correct
-  const user = await User.findOne({ email }).select('+password');
+  const user = await User.findOne({ email })
+    .select('+password')
+    .select('+active');
+
+  if (!user.active) {
+    return next(new AppError('Please verify your Email to continue!', 400));
+  }
 
   if (!user || !(await user.correctPassword(password, user.password))) {
     return next(new AppError('Incorrect email or password', 401));
